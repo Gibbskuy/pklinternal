b60ebd623455c388e163bea9a392af6ebd012f28
Gibbskuy <roslinaelly65445@gmail.com>
2024-06-24 15:24:29 +0700

    tampllate

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\http\Controllers\FrontController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// cek tampilan admin
+Route::get('tes', function () {
+    return view('layouts.admin');
+});
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+ });
+
+// route frontend
+
+Route::get('/', [FrontController::class, 'index']);
+Route::get('contact', [FrontController::class, 'contact']);
+Route::get('shop', [FrontController::class, 'shop']);
+Route::get('cart', [FrontController::class, 'cart']);
+Route::get('checkout', [FrontController::class, 'checkout']);
+Route::get('produktrack', [FrontController::class, 'produktrack']);
+Route::get('about', [FrontController::class, 'about']);
+Route::get('produkdetail', [FrontController::class, 'produkdetail']);
+
+
